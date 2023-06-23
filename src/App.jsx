d81ee11d052353289f7fff3a24ca7499c5fe48aa
fbuonojr <fbuonojr@fordham.edu>
2023-06-23 18:37:38 -0400

    state

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,21 @@ import { useState } from "react";
 
 export default function App() {
   const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false }]
+    });
+  }
 
   return (
     <>
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
           <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item" />
@@ -14,13 +25,17 @@ export default function App() {
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        {todos.map(todo => {
+          return
+          <li>
+            <label>
+              <input type="checkbox" />
+              Item 1
+            </label>
+            <button className="btn btn-danger">Delete</button>
+          </li>
+        })}
+
       </ul>
     </>
   )
