fc294fe20635a468187b88f9888b05330e3cb7fc
fbuonojr <fbuonojr@fordham.edu>
2023-06-23 19:34:37 -0400

    list components

---
+++ src/TodoItem.jsx
@@ -0,0 +1,15 @@
+export function TodoItem({ completed, item, title, toggleTodo, deleteTodo }){
+    return (
+            <li key={todo.id}>
+              <label>
+                <input type="checkbox" checked={todo.completed} 
+                onChange={e => toggleTodo(todo.id, e.target.checked)} 
+                />
+                {todo.title}
+              </label>
+              <button 
+              onClick={() => deleteTodo(todo.id)} 
+              className="btn btn-danger">Delete</button>
+            </li>
+    )
+}
---
+++ src/TodoList.jsx
@@ -0,0 +1,10 @@
+export function TodoList({ todos }){
+    return (
+        <ul className="list">
+        {todos.length === 0 && "No Todos"}
+        {todos.map(todo => {
+        return <TodoItem {...todo} key={todo.id} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+        })}
+      </ul>
+    )
+}
