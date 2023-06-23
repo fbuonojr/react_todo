a8e9437f2ddcc32e682485b0d6bd66f626e38b5d
fbuonojr <fbuonojr@fordham.edu>
2023-06-23 18:43:56 -0400

    toggle todo

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,18 @@ export default function App() {
     setNewItem("");
   }
 
+  function toggleTodo(id, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if(todo.id === id) {
+          return {...todo,completed}
+        }
+
+        return todo
+      })
+    })
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -31,7 +43,7 @@ export default function App() {
           return (
             <li key={todo.id}>
               <label>
-                <input type="checkbox" checked={todo.completed} />
+                <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)}/>
                 {todo.title}
               </label>
               <button className="btn btn-danger">Delete</button>
